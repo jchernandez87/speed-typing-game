6d00e5d04a12e4e26bc874f01e4f391349eb1dce
Juan Carlos <jchernandez827@gmail.com>
2022-08-26 14:46:58 -0500

    Add basic structure and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Speed typing game</h1>
+      <textarea  value="my words here"/>
+      <h4>20 second left</h4>
+      <button>Start</button>
+      <h1>Words count</h1>
     </div>
   );
 }
