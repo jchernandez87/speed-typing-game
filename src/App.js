22f16e2ec0a97a38c8da876c18a63835ae1dc9ef
Juan Carlos <jchernandez827@gmail.com>
2022-08-26 15:47:50 -0500

    add state and effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [words, setWords] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(15);
+  const [start, setStart] = useState(false);
+
+  useEffect(() => {
+    if (start === true) {
+      if (timeRemaining > 0) {
+        setTimeout(() => setTimeRemaining((time) => time - 1), 1000);
+        console.log(timeRemaining);
+      }
+    }
+  }, [start, timeRemaining]);
+
+  const handleChange = (event) => {
+    const { value } = event.target;
+
+    setWords(value);
+  };
+
+  const calculateWords = (text) => {
+    const strArr = text.split(" ");
+    const wordsNum = strArr.filter((word) => word !== "").length;
+
+    console.log(wordsNum);
+    return wordsNum;
+  };
+
+  const startGame = () => setStart(true);
+
   return (
     <div className="App">
-      <h1>Speed typing game</h1>
-      <textarea  value="my words here"/>
-      <h4>20 second left</h4>
-      <button>Start</button>
-      <h1>Words count</h1>
+      <h1>How fast do you type?</h1>
+      <textarea name="words" onChange={handleChange} value={words} />
+      <h4>Time remaining: {timeRemaining}</h4>
+      <button onClick={startGame}>Start</button>
+      <h1>Word count: ???</h1>
     </div>
   );
 }
